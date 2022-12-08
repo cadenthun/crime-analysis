0e7906395c0933b7d41af7ba7cbe7d653d18bdd3
anaghac1234 <114253270+anaghac1234@users.noreply.github.com>
2022-12-08 13:29:53 -0800

    Add files via upload

--- Clean_Data.py
+++ Clean_Data.py
@@ -8,11 +8,11 @@ class data_cleaner:
     '''
     def __init__(self, file):
         '''
-        initializing the instance by reading the data file
+        initializing the data cleaner by reading the data file
         Args:
-            file: a csv data file.
+            file: csv data file.
         Return:
-            None.
+            N/A
         '''
         #do exception handling
         self.df = pd.read_csv(file)
@@ -31,10 +31,10 @@ class data_cleaner:
         '''
         change the name of given columns
         Args:
-            oldName: an array of strings.
-            newName: an array of strings.
+            oldName: an array of strings of current column names.
+            newName: an array of strings of new column names.
         Return:
-            None.
+            None
         '''
         if len(oldName)==len(newName):
             for i in range(len(oldName)):
@@ -43,22 +43,52 @@ class data_cleaner:
             raise ValueError("Make sure there are the same number of new and old column names.")
             
     def onlyDate(self, date_name):
+        '''
+        converts date time format to just dates
+        Args:
+            date_name: String of what the date column is called ("Date")
+        Returns"
+            None. 
+        '''
+        
         self.df[date_name] = pd.to_datetime(self.df[date_name])
 
          
     def delNa(self):
         '''
         delete NA values
+        Args:
+            None - amends data cleaner. 
+        Returns:
+            None
         '''
         self.df = self.df.dropna()
         
     def delRows(self):
-        #once change name works fix the variable name here
+        '''
+        deletes rows with incomplete data. 
+        
+        Args:
+            None - amends data cleaner. 
+            
+        Return:
+            None. 
+        '''
         self.df = self.df[self.df["Sex"] != "X"]
         self.df = self.df[self.df["Sex"] != "H"]
         self.df = self.df[self.df["Descent"] != "X"]
     
     def toNum(self):
+        '''
+        ensures that there are no unknown data types. 
+        converts females and males to 0 and 1 respectively. 
+        also converts descent into a number. 
+        
+        Args:
+            None - amends data cleaner
+        Returns:
+            None. 
+        '''
         if any(self.df["Sex"] == "X"):
             raise TypeError("There are victim's with unknown sex in this dataset.")
         if any(self.df["Sex"] == "H"):
@@ -71,23 +101,16 @@ class data_cleaner:
             self.df['Descent'] = le.fit_transform(self.df['Descent'])
             #change male values that are greater than 0 to 1. 
             #data[data["Vict Sex"] > 0] = 1 this is wrong
-   
-            
-    #accessor methods
-    def showShape(self):
-        return self.df.shape
-        
-    def value_counts(self):
-        return self.df.value_counts()
-    
-    def showHead(self):
-        print(self.df.head)
-        
-    def drop(self, column):
-        self.df.drop([column], axis = 1)
-        
-    def column(self, column_name): #properly returns a panda series.
-        return self.df[column_name]
     
     def createDF(self):
+        '''
+        converts data cleaner into a dataframe
+        
+        Args:
+            None - amends data cleaner. 
+            
+        Return:
+            None. 
+        '''
+        
         return pd.DataFrame(data = self.df)
